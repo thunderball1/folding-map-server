6567a4aff3bc6e029f5af20b59ab984a4dd7a034
fradovan <radovan.fekec@gmail.com>
2015-08-04 18:23:52 +0200

    Packer handled as a NPM package now, removed own module, using GrowingPacker instead of ordinary Packer

--- app/app.js
+++ app/app.js
@@ -1,10 +1,9 @@
 var WebSocketServer = require('websocket').server;
 var http = require('http');
+var binpacking = require('binpacking'); //https://github.com/jsmarkus/node-bin-packing
 var port = 3002;
 
-//var Packer = require('./packer.js');
 var connectedDevices = [];
-
 
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
@@ -90,46 +89,10 @@ function addNewDevice(id, data) {
 }
 
 function packDevices() {
+    var GrowingPacker = binpacking.GrowingPacker;
+    var packer = new GrowingPacker;
+
     connectedDevices.sort(function(a,b) { return (b.h < a.h); });
-    var packer = new Packer(5000, 5000);
     packer.fit(connectedDevices);
     return connectedDevices;
-}
-
-
-Packer = function(w, h) {
-    this.init(w, h);
-};
-
-Packer.prototype = {
-
-    init: function(w, h) {
-        this.root = { x: 0, y: 0, w: w, h: h };
-    },
-
-    fit: function(blocks) {
-        var n, node, block;
-        for (n = 0; n < blocks.length; n++) {
-            block = blocks[n];
-            if (node = this.findNode(this.root, block.w, block.h))
-                block.fit = this.splitNode(node, block.w, block.h);
-        }
-    },
-
-    findNode: function(root, w, h) {
-        if (root.used)
-            return this.findNode(root.right, w, h) || this.findNode(root.down, w, h);
-        else if ((w <= root.w) && (h <= root.h))
-            return root;
-        else
-            return null;
-    },
-
-    splitNode: function(node, w, h) {
-        node.used = true;
-        node.down  = { x: node.x,     y: node.y + h, w: node.w,     h: node.h - h };
-        node.right = { x: node.x + w, y: node.y,     w: node.w - w, h: h          };
-        return node;
-    }
-
 }
