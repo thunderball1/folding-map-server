7b0f2862a70f508d8a4b6abda24226b8f50bc506
fradovan <radovan.fekec@gmail.com>
2015-08-04 18:09:55 +0200

    Packer works - needs to be implemented as a module and configured properly

--- app/app.js
+++ app/app.js
@@ -2,8 +2,8 @@ var WebSocketServer = require('websocket').server;
 var http = require('http');
 var port = 3002;
 
-var Packer = require('./packer.js');
-var connectedDevices = {};
+//var Packer = require('./packer.js');
+var connectedDevices = [];
 
 
 var server = http.createServer(function(request, response) {
@@ -41,10 +41,12 @@ wsServer.on('request', function(request) {
 
         if (isInitialMessage(parsedMessage)) {
             addNewDevice(this.id, parsedMessage);
+            packDevices();
         } else {
             console.log('Received Message: ' + message.utf8Data);
             //connection.sendUTF(message.utf8Data);
-            broadcast(this.id, message.utf8Data)
+            parsedMessage.composition = connectedDevices;
+            broadcast(this.id, JSON.stringify(parsedMessage))
         }
 
     });
@@ -59,8 +61,8 @@ wsServer.on('request', function(request) {
 });
 
 function isInitialMessage(message) {
-    return message.hasOwnProperty('height') && message.
-            hasOwnProperty('width');
+    return message.hasOwnProperty('h') && message.
+            hasOwnProperty('w');
 }
 
 // Broadcast to all open connections
@@ -82,10 +84,52 @@ function sendToConnectionId(connectionID, data) {
 }
 
 function addNewDevice(id, data) {
-    connectedDevices[id] = data;
+    data.id = id;
+    connectedDevices.push(data);
     console.log(connectedDevices);
 }
 
-function packDevices(connectedDevices) {
+function packDevices() {
+    connectedDevices.sort(function(a,b) { return (b.h < a.h); });
+    var packer = new Packer(5000, 5000);
+    packer.fit(connectedDevices);
+    return connectedDevices;
+}
+
+
+Packer = function(w, h) {
+    this.init(w, h);
+};
+
+Packer.prototype = {
+
+    init: function(w, h) {
+        this.root = { x: 0, y: 0, w: w, h: h };
+    },
+
+    fit: function(blocks) {
+        var n, node, block;
+        for (n = 0; n < blocks.length; n++) {
+            block = blocks[n];
+            if (node = this.findNode(this.root, block.w, block.h))
+                block.fit = this.splitNode(node, block.w, block.h);
+        }
+    },
+
+    findNode: function(root, w, h) {
+        if (root.used)
+            return this.findNode(root.right, w, h) || this.findNode(root.down, w, h);
+        else if ((w <= root.w) && (h <= root.h))
+            return root;
+        else
+            return null;
+    },
+
+    splitNode: function(node, w, h) {
+        node.used = true;
+        node.down  = { x: node.x,     y: node.y + h, w: node.w,     h: node.h - h };
+        node.right = { x: node.x + w, y: node.y,     w: node.w - w, h: h          };
+        return node;
+    }
 
 }
