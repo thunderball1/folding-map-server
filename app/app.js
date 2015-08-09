dc4c550eba887dd48029be0ce0bfc8065622b8a4
Adam Vresky <vreskyadam@gmail.com>
2015-08-09 13:10:30 +0200

    logging & docs

--- app/app.js
+++ app/app.js
@@ -11,28 +11,48 @@ winston.add(winston.transports.File, { filename: 'server.log' });
 
 var port = 3002;
 
+/**
+ * Initialize basic HTTP server
+ */
 var server = http.createServer(function(request, response) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
 
+/**
+ * HTTP server listen event
+ */
 server.listen(port, function() {
     console.log('Server is listening on port ' +
         server.address().port.toString());
 });
 
+/**
+ * WebSocket server instance
+ */
 wsServer = new WebSocketServer({ httpServer: server });
-
-//shutDown(wsServer);
-
 winston.log('info', 'Server has been initialized');
 
+/**
+ * Connections
+ *
+ * @type {{}}
+ */
 var connections = {};
 var connectionIDCounter = 0;
 
+/**
+ * Devices Array
+ *
+ * @type {Array}
+ */
 var connectedDevices = [];
 
+/**
+ * Message kinds
+ * @type {{NEW_DEVICE: string, DEVICE_EXISTS: string, REGISTER_DEVICE: string, SET_VIEW: string, NEW_GUID: string, SYNC_COMPOSITION: string, SYNC_VIEW: string}}
+ */
 var KIND = {
     'NEW_DEVICE' : 'NEW_DEVICE',
     'DEVICE_EXISTS' : 'DEVICE_EXISTS',
@@ -73,29 +93,37 @@ wsServer.on('request', function(request) {
             case KIND.NEW_DEVICE:
 
                 this.guid = generateUniqueDeviceKey();
+                winston.log('info', 'New device key generated', this.remoteAddress, this.guid);
 
                 sendToConnectionId(this.id,
                     generateMessage(KIND.NEW_GUID, { guid: this.guid }));
+
+                winston.log('info', 'Sending new GUID to device', this.remoteAddress, this.guid, this.id);
 
                 break;
             case KIND.DEVICE_EXISTS:
                 this.guid = msg.data.guid;
 
+                winston.log('info', 'Device GUID already exists', this.remoteAddress, this.guid);
+
                 break;
             case KIND.REGISTER_DEVICE:
                 addDevice(this.id, this.guid, msg.data);
+                winston.log('info', 'Adding new device', this.remoteAddress, this.guid);
+
                 packDevices();
                 broadcast(undefined,
                     generateMessage(KIND.SYNC_COMPOSITION,
                         connectedDevices));
+
+                winston.log('info', 'Broadcasting new device to all devices');
                 break;
             case KIND.SET_VIEW:
-                console.log('Received Message: ' + msg);
-
                 var tmp = JSON.parse(msg.data);
                 tmp.composition = connectedDevices;
 
                 broadcast(this.id, generateMessage(KIND.SYNC_VIEW, tmp));
+                winston.log('info', 'Broadcasting sync view data to all devices');
 
                 break;
         }
@@ -106,7 +134,7 @@ wsServer.on('request', function(request) {
      * Event when connection is closing
      */
     connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected. ' +
+        winston.log('info', (new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected. ' +
             "Connection ID: " + connection.id);
 
         // Make sure to remove closed connections from the global pool
@@ -230,10 +258,21 @@ function getUTF8Data(message) {
     return JSON.parse(message.utf8Data)
 }
 
+
+/**
+ * Close all connection on WebSocket
+ *
+ * @param webSocket
+ */
 function closeAllConnections(webSocket) {
     webSocket.closeAllConnections();
 }
 
+/**
+ * Shutdown server and kill node process
+ *
+ * @param websocket
+ */
 function shutDown(websocket) {
     winston.log('warn', 'Server will be shutted down');
     websocket.shutDown();
